4f11b0bd0cb7995781c2f15b013d263ac8cadc10
lifengli137 <54550920+lifengli137@users.noreply.github.com>
2023-10-20 15:16:42 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,7 +15,7 @@ while true; do
     file_name="${file_path}/${current_time}.mp4"  
   
     mkdir -p "$file_path"  
-    openRTSP -4 -D 1 -B 10000000 -b 10000000 -t "$rtsp_url" > "$file_name" 2>>/var/log/rtsp/$camera_name  
+    openRTSP -4 -D 1 -B 10000000 -b 10000000 -t "$rtsp_url" > "$file_name" 2>>/dev/null  
   
     if [ -s "$file_name" ]; then  
         file_size=$(du -sh "$file_name" | cut -f1)  
