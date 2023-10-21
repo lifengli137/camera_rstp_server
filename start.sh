278958916cad48f59871de1c07deb11eaf294c3a
lifengli137 <54550920+lifengli137@users.noreply.github.com>
2023-10-20 22:12:53 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -22,7 +22,7 @@ while true; do
     if [ -s "$file_name" ]; then  
         file_size=$(du -sh "$file_name" | cut -f1)  
         echo "${camera_name}_${current_time}.mp4	$file_size"  
-        openssl enc -aes-256-cbc -in ${file_name} -out ${encrypted_file} -pass pass:${pass}${current_time} -salt
+        openssl enc -aes-256-cbc -in ${file_name} -out ${encrypted_file} -pass pass:${pass}${current_time} -salt -md md5
     fi
 
     rm -f "$file_name"  
