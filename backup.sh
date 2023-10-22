0572be0f0f15a61ba1eef3f666fc03a5fc52dcf1
lifengli137 <54550920+lifengli137@users.noreply.github.com>
2023-10-22 16:03:40 -0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -10,7 +10,6 @@ storage_sas_token="$4"
 find $path -type f -print0 | while IFS= read -r -d $'\0' file; do  
     # Upload the file to Azure Blob Storage  
 
-    #azcopy copy --overwrite=false --check-md5 "$file" "https://homeblob137.blob.core.windows.net/enc/${file#$path/}?sp=racwl&st=2023-10-22T22:44:49Z&se=2033-11-02T06:44:49Z&spr=https&sv=2022-11-02&sr=c&sig=I3AJpolboK5EukKb1W5jvdnshArug%2BUr63yPeWRr92c%3D"
     #exit_code=$?  
     echo "$file" 
     echo "https://$storage_account.blob.core.windows.net/$storage_container/${file#$path/}$storage_sas_token"
