3d1e7d44171ddf07040f1f3d2be3d5c0c7a7ec99
lifengli137 <lifengli137@gmail.com>
2023-10-23 12:46:38 -0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,28 +4,33 @@ path="$1"
 storage_account="$2"  
 storage_container="$3"  
 storage_sas_token="$4"  
-azcopy_path="$5"
 
-export PATH=$PATH:$azcopy_path
+if [ -f /tmp/find.tmp ]; then  
+  rm /tmp/find.tmp  
+fi  
 
-# Traverse the folder and process files  
-find $path -type f -print0 | while IFS= read -r -d $'\0' file; do  
-    # Upload the file to Azure Blob Storage  
+while true; do  
+  # Find all files in the directory structure and output the paths to /tmp/find.tmp  
+  find "$path" -type f > /tmp/find.tmp  
+  files=$(cat /tmp/find.tmp)
 
+  for file in $files; do
     relative_path=${file#$path}
-    
-    azcopy cp "$file" "https://$storage_account.blob.core.windows.net/$storage_container/$relative_path?$storage_sas_token" --overwrite=false --put-md5 --check-md5 FailIfDifferent > /dev/null 2>&1  
-    #exit_code=$?
-    # If the upload was successful, remove the local file  
-    #if [ $exit_code -eq 0 ]; then  
-        #rm "$file"  
-     #   echo "File $file uploaded and removed successfully."  >> ./log.txt2>&1  
-    #else  
-    #    echo "Error: File $file failed to upload. Exit code: $exit_code"  >> ./err.txt2>&1  
-    #fi
+      
+    # Perform the azcopy and check the result  
+    azcopy cp "$file" "https://$storage_account.blob.core.windows.net/$storage_container/$relative_path?$storage_sas_token" \
+     --overwrite=false --put-md5 --check-md5 FailIfDifferent --block-blob-tier Cold   
+     
+    # Check if azcopy copy was successful  
+    if [ $? -eq 0 ]; then  
+      # Remove the local file  
+      #rm "$file"  
+      echo "File $file uploaded and removed successfully."
+    else  
+      echo "Error: Failed to transfer $file to Azure Blob Storage"  
+    fi  
+  done
+     
+  # Sleep for 4 hours  
+  sleep 4h  
 done  
-
-# Remove empty directories  
-#find $path -type d -empty -delete  
-echo "find $path -type d -empty -delete"
-  
